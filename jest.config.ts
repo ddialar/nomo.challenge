23b5559be831514d819bb4a7c6096134db511f85
ddialar <ddialar@gmail.com>
2022-10-04 23:36:51 +0100

    chore(repository): update testing configuration.

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,7 @@ export default {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/0y/plf05xnx4jg27zwwlxfr6b200000gn/T/jest_dx",
+  // cacheDirectory: '/private/var/folders/0y/plf05xnx4jg27zwwlxfr6b200000gn/T/jest_dx',
 
   // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
@@ -31,7 +31,7 @@ export default {
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
+  //   '/node_modules/'
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
@@ -39,10 +39,10 @@ export default {
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
+  //   'json',
+  //   'text',
+  //   'lcov',
+  //   'clover'
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
@@ -64,26 +64,22 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json'
-    }
-  },
+  // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  // maxWorkers: '50%',
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
-  //   "node_modules"
+  //   'node_modules'
   // ],
 
   // An array of file extensions your modules use
   moduleFileExtensions: [
-    "js",
-    "jsx",
-    "ts",
-    "tsx"
+    'js',
+    'jsx',
+    'ts',
+    'tsx'
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
@@ -96,7 +92,7 @@ export default {
   // notify: false,
 
   // An enum that specifies notification mode. Requires { notify: true }
-  // notifyMode: "failure-change",
+  // notifyMode: 'failure-change',
 
   // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
@@ -124,11 +120,11 @@ export default {
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
-  //   "<rootDir>"
+  //   '<rootDir>'
   // ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
-  // runner: "jest-runner",
+  // runner: 'jest-runner',
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
@@ -143,7 +139,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -153,13 +149,13 @@ export default {
 
   // The glob patterns Jest uses to detect test files
   // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  //   '**/__tests__/**/*.[jt]s?(x)',
+  //   '**/?(*.)+(spec|test).[tj]s?(x)'
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-    "/node_modules/"
+    '/node_modules/'
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
@@ -169,21 +165,21 @@ export default {
   // testResultsProcessor: undefined,
 
   // This option allows use of a custom test runner
-  // testRunner: "jest-circus/runner",
+  // testRunner: 'jest-circus/runner',
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  // testURL: "http://localhost",
+  // testURL: 'http://localhost',
 
-  // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
-  // timers: "real",
+  // Setting this value to 'fake' allows the use of fake timers for functions such as 'setTimeout'
+  // timers: 'real',
 
   // A map from regular expressions to paths to transformers
-  transform: { '^.+\\.ts|tsx$': 'ts-jest' },
+  transform: { '^.+\\.ts|tsx$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
+  //   '/node_modules/',
+  //   '\\.pnp\\.[^\\/]+$'
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
