64663db9386efba1d31d2b9f8544f3c888a07bba
ddialar <ddialar@gmail.com>
2022-10-04 22:39:58 +0100

    chore(types): define the Publication type.

---
+++ src/@types/publication.d.ts
@@ -0,0 +1,12 @@
+interface Publication {
+  id: string;
+  thumbnail: string;
+  thumbnail_width: number;
+  thumbnail_height: number;
+  created: number;
+  title: string;
+  author: string;
+  score: number;
+  num_comments: number;
+  permalink: string;
+}
