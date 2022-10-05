40fa0380e626934d47fb3762d741d03309cdfc89
ddialar <ddialar@gmail.com>
2022-10-05 07:24:06 +0100

    styles(layouts): update the Main block styles.

--- src/layouts/PageLayout/components/Main/Main.styled.tsx
+++ src/layouts/PageLayout/components/Main/Main.styled.tsx
@@ -4,6 +4,6 @@ export const MainStyled = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  flex-grow: 1;
+  height: calc(100vh - 48px - 64px);
   background-color: #dae0e6;
 `
