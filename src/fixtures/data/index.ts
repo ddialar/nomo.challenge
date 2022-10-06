6c6373dba6ba427ecc638bee98b806b9cafbf892
ddialar <ddialar@gmail.com>
2022-10-06 13:07:22 +0100

    chore(fixtures): include new methods in order to retrieve raw expected API responses.

--- src/fixtures/data/index.ts
+++ src/fixtures/data/index.ts
@@ -4,6 +4,13 @@ import newSubreddit from '../assets/new.json'
 import topSubreddit from '../assets/top.json'
 
 export const getControversialSubredditFixture = () => [...controversialSubreddit.data.children.map(({ data }) => data)] as Array<Publication>
+export const getRawControversialSubredditFixture = () => ({ ...controversialSubreddit })
+
 export const getHotSubredditFixture = () => [...hotSubreddit.data.children.map(({ data }) => data)] as Array<Publication>
+export const getRawHotSubredditFixture = () => ({ ...hotSubreddit })
+
 export const getNewSubredditFixture = () => [...newSubreddit.data.children.map(({ data }) => data)] as Array<Publication>
+export const getRawNewSubredditFixture = () => ({ ...newSubreddit })
+
 export const getTopSubredditFixture = () => [...topSubreddit.data.children.map(({ data }) => data)] as Array<Publication>
+export const getRawTopSubredditFixture = () => ({ ...topSubreddit })
